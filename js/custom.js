7272f7d60abbfb9eaec2de1e0080765d90f6f5da
Corina Acosta <caligirlcori@gmail.com>
2014-01-27 12:02:19 -0500

    Twitter Feed widget added

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,4 @@
 $( document ).ready(function() {
-  
-
-  $.getJSON("https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=iamcorinaacosta&count=5", function( data ) {
-  
-  var items = [];
-
-  console.log(data)
-  
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "tweets",
-    html: items.join( "" )
-  }).appendTo( ".tweets-container" );
-
-});
-
 
   
 });
