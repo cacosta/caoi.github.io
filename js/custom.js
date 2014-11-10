8daeec5765201f30468d6701e4c5c7ea0ca92221
Corina Acosta <cacosta@berklee.edu>
2014-11-10 17:23:33 -0500

    Portfolio section added

--- js/custom.js
+++ js/custom.js
@@ -13,17 +13,26 @@ $( document ).ready(function() {
     }
   });
 
-  jQuery(window).scroll(function(){
-        if (jQuery(this).scrollTop() > 100) {
-            jQuery('.scrollup').fadeIn();
-        } else {
-            jQuery('.scrollup').fadeOut();
+  $(window).scroll(function(){
+    if (jQuery(this).scrollTop() > 100) {
+      jQuery('.scrollup').fadeIn();
+    } else {
+      jQuery('.scrollup').fadeOut();
     }
-});
-// scroll-to-top animate
-jQuery('.scrollup').click(function(){
+  });
+  // scroll-to-top animate
+  $('.scrollup').click(function(){
     jQuery("html, body").animate({ scrollTop: 0 }, 600);
-        return false;
-    });
+    return false;
+  });
+
+  $('.more').on('click', function(){
+    $('.more').fadeOut();
+    $(this).next().slideToggle();
+  });
+  $('.less').on('click', function(){
+    $(this).parent().slideToggle();
+    $('.more').fadeIn();
+  });
 
 });
